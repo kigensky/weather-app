d49fb2a116ce35adfa8d5ee9153fa6e7d210cdc7
kigensky <vickigen@gmail.com>
2021-10-23 13:30:01 +0300

    added a class

--- weather_app/models.py
+++ weather_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.enums import Choices
 
-# Create your models here.
+Days = (
+    (0, "Sunday"),
+    (1, "Monday"),
+    (2, "Teusday"),
+    (3, "Wednesday"),
+    (4, "Thursday"),
+    (5, "Friday"),
+    (6, "Saturday")
+)
+
+class Measurements(models.Model):
+    Days_of_the_week = models.IntegerField(choices=Days, default=0)
+    Rainfall = models.FloatField()
+    Day_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-Day_created']
+
+
